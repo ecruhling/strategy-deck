56ae547c46225ede5f5923120580966fb1cda9bc
erik <ecruhling@gmail.com>
2023-02-16 12:00:33 -0600

    download to PDF; finally
    Signed-off-by: ecruhling <erik@resourceatlanta.com>

--- assets/build/plugin-view.asset.php
+++ assets/build/plugin-view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-api-fetch', 'wp-element'), 'version' => '8658baa3a9883b11a7d2');
+<?php return array('dependencies' => array('wp-api-fetch', 'wp-element'), 'version' => '40ae739eda78f62215bd');
--- assets/src/plugin-view.js
+++ assets/src/plugin-view.js
@@ -1,9 +1,7 @@
 // Runs just on the front end.
 import FrontendDeckCard from './block/view';
 import { render } from '@wordpress/element';
-import 'jspdf';
-import html2canvas from 'html2canvas';
-import html2pdf from 'html2pdf.js';
+import html2PDF from 'jspdf-html2canvas';
 
 // select all the deck cards
 const deckCardClass = '.wp-block-strategydeck-deck-card',
@@ -21,59 +19,24 @@ deckCards.forEach( ( deckCard ) => {
 	render( <FrontendDeckCard dataAttributes={ attributes } />, deckCard );
 } );
 
-// function printPDF() {
-// 	const pdf = new jsPDF( {
-// 		orientation: 'landscape',
-// 		unit: 'pt',
-// 		format: 'ledger',
-// 		putOnlyUsedFonts: false,
-// 	} );
-//
-// 	pdf.html( document.body, {
-// 		html2canvas: {
-// 			scale: 0.9,
-// 		},
-// 		callback() {
-// 			// pdf.save( 'deck.pdf' );
-// 			const iframe = document.createElement( 'iframe' );
-// 			iframe.setAttribute(
-// 				'style',
-// 				'position:fixed;right:0; top:0; height:100vh; width:50vw; z-index: 9999;'
-// 			);
-// 			document.body.appendChild( iframe );
-// 			iframe.src = pdf.output( 'datauristring' );
-// 		},
-// 	} );
-// }
-
 // click on #deck-print button
 document.getElementById( 'deck-print' ).addEventListener( 'click', () => {
 	// the element to print - entire body; the buttons have 'data-html2canvas-ignore' attributes, so they are ignored.
 	const body = document.body;
 
-	// the options to pass to html2pdf()
-	const opt = {
-		filename: document.title + '.pdf',
+	html2PDF( body, {
 		html2canvas: {
-			scale: 0.9,
+			scale: 1,
 			foreignObjectRendering: true,
+			scrollX: 0,
+			scrollY: -window.scrollY,
 		},
 		jsPDF: {
 			orientation: 'landscape',
-			unit: 'in',
-			format: 'ledger',
+			// format: 'letter',
+			format: [ body.offsetWidth, body.offsetHeight + 150 ],
 		},
-	};
-
-	// create PDF and save it
-	html2pdf().set( opt ).from( body ).save();
+		imageType: 'image/jpeg',
+		output: document.title + '.pdf',
+	} );
 } );
-
-// testing canvas by appending it to the document
-// html2canvas( document.body, {
-// 	foreignObjectRendering: true,
-// 	width: document.documentElement.scrollWidth,
-// 	height: document.documentElement.scrollHeight,
-// } ).then( function ( canvas ) {
-// 	document.body.appendChild( canvas );
-// } );
